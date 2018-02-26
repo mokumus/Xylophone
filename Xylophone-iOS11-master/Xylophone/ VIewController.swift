d981bd4d9c324eb5e51b0ab8b8d9b6f10c8191f3
Flamong Butterscotch <rocketpolka@gmail.com>
2018-02-26 03:29:55 +0300

    Add files via upload

---
+++ Xylophone-iOS11-master/Xylophone/ VIewController.swift
@@ -0,0 +1,31 @@
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+	
+	var audioPlayer : AVAudioPlayer!
+	let soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+		
+		playSound(note: soundArray[sender.tag - 1])
+    }
+	
+	func playSound(note: String){
+		let soundURL = Bundle.main.url(forResource: note, withExtension: "wav")
+		
+		do{
+			audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+		} catch { print(error) }
+		
+		audioPlayer.play()
+	}
+  
+
+}
+
